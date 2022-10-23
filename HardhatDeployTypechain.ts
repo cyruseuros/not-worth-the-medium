04364e65261ea6b6a81b1eaf8eb9ab4017df8a22
Uros Perisic <uros.m.perisic@gmail.com>
2022-10-23 11:38:50 +0800

    Hardhat-deploy Typechain

---
+++ HardhatDeployTypechain.ts
@@ -0,0 +1,26 @@
+import type { DeployFunction } from 'hardhat-deploy/types'
+import type { HardhatRuntimeEnvironment } from 'hardhat/types'
+import type { Contract, Contract__factory } from '../typechain-types'
+
+const deploy: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
+  const { deployments, getNamedAccounts } = hre
+  const { deploy, execute } = deployments
+  const accounts = await getNamedAccounts()
+
+  const deployArgs: Parameters<Contract__factory['deploy']> = ['foo']
+  await deploy('Store', {
+    from: accounts.deployer,
+    args: deployArgs,
+  })
+
+  const executeArgs: Parameters<Contract['fn']> = ['bar']
+  execute(
+    'Contract',
+    { from: accounts.deployer },
+    'fn',
+    ...executeArgs,
+  )
+}
+
+deploy.tags = ['Contract']
+export default deploy
